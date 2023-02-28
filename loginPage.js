580ac6f33e98e33d1740b5765539a7f09b63b7a1
Saipavanganesh <pavanganeshms@gmail.com>
2023-02-28 22:03:15 +0530

    Added displaypage

--- loginPage.js
+++ loginPage.js
@@ -1,3 +1,9 @@
 document.getElementById("submit").addEventListener("click",function(event){
-    console.log("Login success");
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    localStorage.setItem("Name",name);
+    localStorage.setItem("Email",email);
+    // alert("Login success");
  })
+
+//  document.getElementById("submit").onclick = window.location.href="https://www.facebook.com";
